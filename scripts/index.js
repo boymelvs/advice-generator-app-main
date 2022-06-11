004bbdaa21ba08746e7523be36e12a9ae4cf99a5
MELVIN <19977036+boymelvs@users.noreply.github.com>
2022-06-11 14:43:18 +0800

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,6 @@
 const getSubmitBtn = document.querySelector(".submit_btn");
 const getAdviceIdContainer = document.querySelector(".advice_id");
 const getAdviceTextContainer = document.querySelector(".advice_text");
-const getLoadingContainer = document.querySelector(".loading");
 
 const getAdvice = async () => {
    const res = await fetch("https://api.adviceslip.com/advice");
